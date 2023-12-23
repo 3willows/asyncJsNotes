3100109bbe9eddff392488272fd5b5d5713022e2
Jasper Wong <wlcjasper@gmail.com>
2023-12-23 18:33:29 +0800

    fix typo

--- Promises.js
+++ Promises.js
@@ -1,43 +1,45 @@
 //Run in a JS playground 
 
-// Callbacks demonstration
+// Promise demonstration
 
-const trial = (func, value) => {
-  (value) => {
-    if (value > 0.5){
-      console.log('more than half')
+const trial = () => {
+  const random = Math.random();
+  return new Promise((resolve, reject) =>{
+    if (random > 0.5){
+      resolve('more than half!')
     }
     else
-    console.log('less than half')
-  }
+    reject('less than half!')
+  })
 }
 
 // each call iniitates new promise, 
 // so some may be rejected and some accepted
 
-let a = trial()
-let b = trial()
-let c = trial()
-let d = trial()
+// let a = trial()
+// let b = trial()
+// let c = trial()
+// let d = trial()
 
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
 
 // Promises can be chained together
+// Res and reject can be used to pass a message
 
 trial()
-  .then(res => {
-    console.log(res + '! success 1!')
+  .then(la => {
+    console.log(la + ' success 1!')
     return trial()
   })
   .then(res => {
-    console.log(res + '! success 2!')
+    console.log(res + ' success 2!')
     return trial()
   })
   .then(res => {
-    console.log(res + '! success 3!')
+    console.log(res + ' success 3!')
     return trial()
   })
   .catch(err => console.log(err + ' at some point'))
