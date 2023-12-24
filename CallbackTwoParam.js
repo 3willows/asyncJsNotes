4fdd0939be92505c810b44e0a2cd9fc7aca40b63
Jasper Wong <wlcjasper@gmail.com>
2023-12-24 14:03:05 +0800

    complete 2 params

--- CallbackTwoParam.js
+++ CallbackTwoParam.js
@@ -11,23 +11,35 @@ function trial2 (callback) {
 
 // Call once
 
-const simpleReturn = (err, res) =>{
+const simpleReturn = (err, res) => {
   if (res) {
     return res
-  } else
-  return err
+  } else return err
 }
 
-console.log(trial2(simpleReturn))
+// console.log(trial2(simpleReturn))
 
 // Try to make recursive
 
 function trial2R (callback) {
   let random = Math.random()
   if (random > 0.5) {
+    console.log('heads')
     trial2R(callback)
     return callback(null, 'heads')
   } else {
+    console.log(`tails!  It's over`)
     return callback('tails', null)
   }
 }
+
+const loop = () => {
+  trial2R(simpleReturn)
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log('START A ROUND')
+  loop()
+}
+
+1
